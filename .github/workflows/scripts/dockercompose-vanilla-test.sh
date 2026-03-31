0d2c0fb57730522b483a25aa852bcdcf205f8014
Marcus Mödinger <55532419+mreise@users.noreply.github.com>
2026-03-31 16:06:23 +0200

    add docker compose setup test (#43)
    * add docker compose setup test
    
    * debug docker compose setup
    
    Comment out the docker compose command to prevent it from running in the background.
    
    Signed-off-by: Marcus Mödinger <55532419+mreise@users.noreply.github.com>
    
    * fix docker compose setup test
    
    * fix docker compose setup test
    
    ---------
    
    Signed-off-by: Marcus Mödinger <55532419+mreise@users.noreply.github.com>

---
+++ .github/workflows/scripts/dockercompose-vanilla-test.sh
@@ -0,0 +1,63 @@
+# SPDX-FileCopyrightText: 2025 Mercedes-Benz Group AG and Mercedes-Benz AG
+#
+# SPDX-License-Identifier: Apache-2.0
+
+#!/bin/bash
+set -euo pipefail
+
+isContainerRunning() {
+    local container_name=$1
+    health=$(docker inspect --format "{{json .State.Health }}" $container_name | jq -r .Status) # healthy | unhealthy | starting
+    status=$(docker inspect --format "{{json .State.Status }}" $container_name | jq -r .) # running | exited
+    if [ "$health" = "healthy" ] && [ "$status" = "running" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+isContainerExitedSuccessful() {
+    local container_name=$1
+    status=$(docker inspect --format "{{json .State.Status }}" $container_name | jq -r .) # running | exited
+    exit_code=$(docker inspect --format "{{.State.ExitCode}}" $container_name) # 0 | 1 | ...
+    if [ "$status" = "exited" ] && [ $exit_code -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+generateSelfSignedCertificate() {
+  if [ -f "certs/localhost.pem" ] && [ -f "certs/localhost-key.pem" ]; then
+    echo "TLS certificate already exists, skipping generation."
+    return
+  fi
+  mkdir -p certs && chmod 757 certs
+  openssl req -x509 -newkey rsa:4096 -keyout certs/localhost-key.pem -out certs/localhost.pem -days 1 -nodes -subj "/CN=localhost" >> /dev/null 2>&1
+}
+
+generateSelfSignedCertificate
+docker compose up -d --build >> /dev/null 2>&1
+container_names=$(docker ps --format "{{.Names}}" --filter "name=disuko")
+remaining_container_count=$(echo "$container_names" | wc -l)
+container_checked_count=0
+timeout=180
+start_time=$(date +%s)
+while [ $container_checked_count -lt $remaining_container_count ]; do
+echo "$container_checked_count from $remaining_container_count containers are checked..."
+for container_name in $container_names; do
+  if isContainerRunning "$container_name" || isContainerExitedSuccessful "$container_name"; then
+    container_checked_count=$((container_checked_count + 1))
+    container_names=$(echo "$container_names" | grep -v $container_name || true)
+  fi
+  sleep 5
+done
+if [ $(( $(date +%s) - start_time )) -ge $timeout ]; then
+  echo "Timeout ${timeout}s reached while waiting for containers to become healthy."
+  docker ps
+  docker compose down >> /dev/null 2>&1
+  exit 1
+fi
+done
+echo "All $remaining_container_count containers are checked."
+docker compose down >> /dev/null 2>&1
